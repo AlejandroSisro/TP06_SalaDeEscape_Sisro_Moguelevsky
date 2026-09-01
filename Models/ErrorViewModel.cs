81e050bf916b4590dff41690c44adbc3bbf87078
ALEJANDRO MOISES SISRO MELES <50434059@ort>
2026-09-01 08:14:03 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP06_SalaDeEscape_Sisro_Moguelevsky.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
