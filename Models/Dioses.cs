acb98413d8d59461826079dc42c009528eced767
ALEJANDRO SISRO <50434059@est.ort.edu.ar>
2026-09-10 08:12:35 -0300

    Add files via upload

---
+++ Models/Dioses.cs
@@ -0,0 +1,10 @@
+namespace TP06_SalaDeEscape_Sisro_Moguelevsky.Models
+{
+    public class Dioses
+    {
+        public int IdDios { get; set; }
+        public string Nombre { get; set; }
+        public string FotoDios { get; set; }
+        public string Dialogo { get; set; }
+    }
+}
