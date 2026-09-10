3eb12174ae22576413ebdd92500b2293d898326a
ALEJANDRO SISRO <50434059@est.ort.edu.ar>
2026-09-10 08:12:52 -0300

    Update Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,11 +6,6 @@ namespace TP06_SalaDeEscape_Sisro_Moguelevsky.Models
     {
         public int Id { get; set; }
         public string nombreUsuario { get; set; }
-        public string contraseña { get; set; }
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public int IdBendicion { get; set; }
-        public int IdMaldicion { get; set; }
         public int Sala { get; set; }
     }
 }
